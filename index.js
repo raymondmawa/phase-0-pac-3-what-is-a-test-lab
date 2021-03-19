587567e4a054e4d8d90e1c7f955e721a8a9ebca5
Raymond Mawanda <rmawanda3@gmail.com>
2021-03-19 00:00:49 -0700

    Done with Assignment

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const height = 39;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+const name = "Joe";
+const height = 74;
+const message = `${name} is ${height} inches tall`;
+
+module.exports = {name, height, message};
+
+console.log(message);
